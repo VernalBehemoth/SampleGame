531a8878a9b9b4eb6360aaf09465590a2b9bd5ed
Graham <gvoisey@live.com>
2021-05-21 16:16:05 +0200

    [Task#4] Only teleport specific gameobjects
    Control which games objects to teleport through a list on the teleport script.

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -1,26 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
     [SerializeField]
+    [Tooltip("Where to teleport the collider to")]
     private Transform targetTransofrm;
+
+    [SerializeField]
+    [Tooltip("Only teleport things in this list, if empty teleport everything (including chickens)")]
+    private List<Collider2D> teleportMeCollider;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerEnter2D(Collider2D collider) {    
-        collider.gameObject.transform.position = targetTransofrm.position;
+    private void OnTriggerEnter2D(Collider2D collider) {          
+        if(teleportMeCollider.Any() ? teleportMeCollider.Any(x=>x == collider) : true)
+            collider.gameObject.transform.position = targetTransofrm.position;
     }
         
 }
