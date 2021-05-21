c38e1e135d9dd219cb58831459afc871103b1698
Graham <gvoisey@live.com>
2021-05-21 08:50:15 +0200

    [Task#1] Character movement
    Add character movement to the character controller script

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -7,13 +7,23 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private float speed = 2f;
 
     private Rigidbody2D _rigidbody2D;
+    private float horizontalInput;
+    private float verticalInput;
 
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
     }
-    
+
+    void Update()
+    {
+        //Track player input on update for better responsivness
+        horizontalInput = Input.GetAxisRaw("Horizontal"); 
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
     private void FixedUpdate()
     {
+        _rigidbody2D.velocity = new Vector2(horizontalInput * speed, verticalInput * speed);
     }
 }
